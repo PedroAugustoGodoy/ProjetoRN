82179daedea9b29e01e3272db2fbd6fcd3777056
PedroAugustoGodoy <d2021005227@unifei.edu.br>
2023-12-07 10:29:25 -0300

    tanto faz

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import Tela1 from './Tela1';
-import Tela2 from './Tela2';
-import Tela3 from './Tela3';
-import Tela4 from './Tela4';
+import Tela1 from './src/components/Tela1';
+import Tela2 from './src/components/Tela2';
+import Tela3 from './src/components/Tela3';
+import Tela4 from './src/components/Tela4';
 
 const AppNavigator = createStackNavigator(
   {
